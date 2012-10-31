fecb8bb0a74b94c2500ef769d22d4256bc6a81a2
Chris Knadler <takeshi91k@gmail.com>
2012-10-30 22:35:55 -0700

    Changes to FileUtils from system in actions module

--- lib/rcomp/actions.rb
+++ lib/rcomp/actions.rb
@@ -3,21 +3,21 @@ class RComp
     
     def rm_rf(directory)
       if File.exist? directory
-        system "rm -rf #{directory}"
+        FileUtils.rm_rf directory
         say "Removed #{directory} and all subdirectories"
       end
     end
 
     def rm(file)
       if File.exist? file
-        system "rm #{file}"
+        FileUtils.rm file
         say "Removed #{file}"
       end
     end
 
     def mkdir(path)
       unless File.exist? path
-        system "mkdir #{path}" 
+        FileUtils.mkdir path
         say "Created #{path}"
       end
     end
